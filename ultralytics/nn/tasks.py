8fa268e7064fe0c97514c28f68669782a30545f0
ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>
2025-02-15 00:39:48 +0530

    Update tasks.py
    Signed-off-by: ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -988,6 +988,9 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             SCDown,
             C2fCIB,
             BiFPN,
+            DepthwiseConvBlock,
+            ConvBlock,
+            BiFPNBlock,
         }
     )
     repeat_modules = frozenset(  # modules with 'repeat' arguments
