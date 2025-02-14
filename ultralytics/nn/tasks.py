806e6266cd1d28a3ce4bd99544663368adc657f4
ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>
2025-02-14 16:07:13 +0530

    Update tasks.py
    added DepthwiseConvBlock, ConvBlock and BiFPNBlock at the top _all
    
    Signed-off-by: ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -63,6 +63,10 @@ from ultralytics.nn.modules import (
     TorchVision,
     WorldDetect,
     v10Detect,
+    DepthwiseConvBlock,
+    ConvBlock,
+    BiFPNBlock,
+
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
