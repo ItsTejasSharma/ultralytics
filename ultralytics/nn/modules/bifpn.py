2b2cc8b75d9bbaf6906c9244ba58ad2d90951ecb
ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>
2025-02-14 21:22:36 +0530

    Update bifpn.py
    Signed-off-by: ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>

--- ultralytics/nn/modules/bifpn.py
+++ ultralytics/nn/modules/bifpn.py
@@ -11,30 +11,27 @@ class BiFPN(nn.Module):
         self.p3 = nn.Conv2d(size[0], feature_size, kernel_size=1, stride=1, padding=0)
         self.p4 = nn.Conv2d(size[1], feature_size, kernel_size=1, stride=1, padding=0)
         self.p5 = nn.Conv2d(size[2], feature_size, kernel_size=1, stride=1, padding=0)
-
+        
         # p6 is obtained via a 3x3 stride-2 conv on C5
         self.p6 = nn.Conv2d(size[2], feature_size, kernel_size=3, stride=2, padding=1)
-
+        
         # p7 is computed by applying ReLU followed by a 3x3 stride-2 conv on p6
         self.p7 = ConvBlock(feature_size, feature_size, kernel_size=3, stride=2, padding=1)
 
-        self.bifpns = nn.ModuleList([BiFPNBlock(feature_size) for _ in range(num_layers)]) # Use ModuleList
-
-
+        bifpns = []
+        for _ in range(num_layers):
+            bifpns.append(BiFPNBlock(feature_size))
+        self.bifpn = nn.Sequential(*bifpns)
+    
     def forward(self, inputs):
         c3, c4, c5 = inputs
-
+        
         # Calculate the input column of BiFPN
-        p3_x = self.p3(c3)
+        p3_x = self.p3(c3)        
         p4_x = self.p4(c4)
         p5_x = self.p5(c5)
         p6_x = self.p6(c5)
         p7_x = self.p7(p6_x)
-
+        
         features = [p3_x, p4_x, p5_x, p6_x, p7_x]
-
-        # Iterate through BiFPN blocks, passing the *list* of features
-        for bifpn_block in self.bifpns:
-            features = bifpn_block(features)  # Pass the entire list
-
-        return features  # Return the list of feature maps
+        return self.bifpn(features)
