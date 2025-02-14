2380a1507f1cc87cd14d1358c7f7cbedb9461732
ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>
2025-02-14 22:32:51 +0530

    Update tasks.py
    Signed-off-by: ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1021,15 +1021,15 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 with contextlib.suppress(ValueError):
                     args[j] = locals()[a] if a in locals() else ast.literal_eval(a)
         n = n_ = max(round(n * depth), 1) if n > 1 else n  # depth gain
-       if m in base_modules:
-    c1, c2 = ch[f], args[0]
+    if m in base_modules:
+        c1, c2 = ch[f], args[0]
     if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
         c2 = make_divisible(min(c2, max_channels) * width, 8)
     if m is C2fAttn:  # set 1) embed channels and 2) num heads
         args[1] = make_divisible(min(args[1], max_channels // 2) * width, 8)
         args[2] = int(max(round(min(args[2], max_channels // 2 // 32)) * width, 1) if args[2] > 1 else args[2])
 
-    args = [c1, c2, *args[1:]]
+        args = [c1, c2, *args[1:]]
     if m in repeat_modules:
         args.insert(2, n)  # number of repeats
         n = 1
