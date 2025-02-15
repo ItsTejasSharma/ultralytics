a176cea7aae4b2b1eea48f190f0088081b427e7c
ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>
2025-02-15 16:06:41 +0530

    Update loss.py
    Signed-off-by: ItsTejasSharma <159180856+ItsTejasSharma@users.noreply.github.com>

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -8,6 +8,7 @@ from ultralytics.utils.metrics import OKS_SIGMA
 from ultralytics.utils.ops import crop_mask, xywh2xyxy, xyxy2xywh
 from ultralytics.utils.tal import RotatedTaskAlignedAssigner, TaskAlignedAssigner, dist2bbox, dist2rbox, make_anchors
 from ultralytics.utils.torch_utils import autocast
+from ultralytics.nn.modules import Detect
 
 from .metrics import bbox_iou, probiou
 from .tal import bbox2dist
